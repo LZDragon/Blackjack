910dcc92dc4c122c2ff00d29e0f8743431c83dd1
LZDragon <lazydragon193@gmail.com>
2024-02-27 20:57:48 -0500

    added continue property

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,9 +4,15 @@ using System.Collections.Generic;
 
 public class Player
 {
+    private string name;
     private List<Card> hand;
     private bool continuePlaying;
     private int handValue;
+    public bool ContinuePlaying
+    {
+        get => continuePlaying;
+        set => continuePlaying = value;
+    }
 
     public int EvaluateHand()
     {
